b9708aec4776f2438b7e0b6e22bfa32e585c4f43
pranshu2610 <b218030@iiit-bh.ac.in>
2020-06-30 18:23:26 +0530

    Updates

---
+++ precache-manifest.ecacbe6c52e0d91a6285339cd2decf3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d765e2a179301584c26ddda207eebb44",
+    "url": "/triceria-app/index.html"
+  },
+  {
+    "revision": "bf3992fcfb6faf4ed8d1",
+    "url": "/triceria-app/static/css/main.5c180b77.chunk.css"
+  },
+  {
+    "revision": "d2d97bb5bfc413e3e99d",
+    "url": "/triceria-app/static/js/2.2fd5ffcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triceria-app/static/js/2.2fd5ffcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3992fcfb6faf4ed8d1",
+    "url": "/triceria-app/static/js/main.e083dc18.chunk.js"
+  },
+  {
+    "revision": "5512285ffb00919f3e61",
+    "url": "/triceria-app/static/js/runtime-main.f80792a7.js"
+  }
+]);
